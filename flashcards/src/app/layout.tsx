3f72503a26d924624dc8b3fda80f791da90db77b
vojtiikkxdd <vojtarazima@gmail.com>
2025-01-18 21:19:03 +0100

    lamo edituju header

--- flashcards/src/app/layout.tsx
+++ flashcards/src/app/layout.tsx
@@ -1,14 +1,12 @@
 import type { Metadata } from "next";
-import { Roboto_Mono, Poppins, JetBrains_Mono } from "next/font/google";
+import { Geist_Mono, Poppins, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import { User } from "lucide-react";
 import Link from "next/link";
 
-const robotoMono = Roboto_Mono({
-  variable: "--font-roboto-mono",
+const geistMono = Geist_Mono({
   subsets: ["latin"],
-  weight: ["700", "400"],
-  display: "swap"
+  weight: ["900", "800", "700", "600", "500", "400"],
 });
 
 const poppins = Poppins({
@@ -28,14 +26,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.className} min-h-screen flex flex-col antialiased relative h-full w-full bg-gray-900`}>
-        <div className="absolute inset-0 bg-cyan-300 bg-[size:20px_20px] opacity-20 blur-[100px]"></div>
-        <nav className="p-5 flex flex-row justify-between">
-          <Link href={"/"} className="flex">
+      <body className={`${geistMono.className} min-h-screen flex flex-col antialiased relative h-full w-full bg-gray-900`}>
+        <div className="absolute inset-0 bg-[#14837d] bg-[size:20px_20px] opacity-50 blur-[100px]"></div>
+        <nav className="p-5 flex flex-row justify-between border-b-2 border-dashed border-white bg-[#0c0c0c81]">
+          <Link href={"/"} className="flex cursor-pointer">
             <div className="w-10 h-10 text-2xl">x{/*<Image src={""} alt={""} />*/}</div>
             <h1 className="font-[Poppins] text-4xl">FlashCards</h1>
           </Link>
-          <Link href="/profile" className="ml-auto flex justify-center items-center">
+          <Link href="/profile" className="cursor-pointer ml-auto flex justify-center items-center">
             <User size={30} />
           </Link>
         </nav>
--- flashcards/src/app/page.tsx
+++ flashcards/src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <main className="flex flex-1 flex-col">
       <div className="flex absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 m-auto justify-center items-center">
-        <p className="text-[#fff] text-5xl font-semibold relative">
-          <Link href="/profile" className="bg-clip-text from-[#2592da] to-[#da25d4] bg-gradient-to-r text-transparent hover:font-extrabold duration-75 transition-all ease-in-out">
+        <p className="text-[#fff] text-5xl font-medium relative">
+          <Link href="/profile" className="bg-clip-text from-[#2592da] to-[#da25d4] bg-gradient-to-r text-transparent hover:font-extrabold duration-200 transition-all ease-in-out">
             Log-in
           </Link> and start learning with FlashCards!
 
