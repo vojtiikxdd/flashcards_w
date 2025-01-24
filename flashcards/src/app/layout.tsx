1b03b025c5101f53e58340600f50899514b2bbde
vojtiikkxdd <vojtarazima@gmail.com>
2025-01-24 14:19:22 +0100

    adding input for search to header

--- flashcards/src/app/layout.tsx
+++ flashcards/src/app/layout.tsx
@@ -30,11 +30,16 @@ export default function RootLayout({
       <body className={`${geistMono.className} min-h-screen flex flex-col antialiased relative h-full w-full bg-gray-900`}>
         <div className="pointer-events-none absolute inset-0 bg-[#25153d] bg-[size:20px_20px] opacity-50 blur-[100px]"></div>
         <nav className={` ${poppins.className}
-        p-5 flex flex-row justify-between border-b-2 border-dashed border-white bg-[#0c0c0c81] font-bold`}>
+        p-5 flex flex-row justify-evenly border-b-2 border-dashed border-white bg-[#0c0c0c81] font-bold`}>
           <Link href={"/"} className="flex gap-x-2 cursor-pointer w-fit h-10 text-2xl">
             <Image src="/logo.png" alt="idk" width={75} height={350} />
             <h1 className="font-[Poppins] text-4xl">FlashCards</h1>
           </Link>
+          <div className="flex flex-row mx-auto bg-[#0c0c0c81] w-[30rem] rounded-2xl border-solid border-white border-4 pl-4">
+            {/*najit ikonu lupy a přidat ji */}
+            
+            <input type="text" className="bg-[#0c0c0c81] placeholder:italic" placeholder="Co máte na mysli?" />
+          </div>
           <Link href="/profile" className="cursor-pointer ml-auto flex justify-center items-center">
             <User size={30} />
           </Link>
