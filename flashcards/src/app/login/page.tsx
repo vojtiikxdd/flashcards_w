9210370af9887b6c31eea25543f637c031d96fdb
vojtiikkxdd <vojtarazima@gmail.com>
2025-02-06 22:03:26 +0100

    adding createNew for Mario to make page to create new flashcards

--- flashcards/src/app/login/page.tsx
+++ flashcards/src/app/login/page.tsx
@@ -1,7 +1,7 @@
 export default function Login() {
     return (
         <main>
-            <form className="flex flex-col absolute top-[45%] left-1/2 -translate-y-1/2 -translate-x-1/2 m-auto border-2 border-[#ffffffb5] border-dashed rounded-3xl w-[55rem] h-[30rem] bg-[#1f1f1f3d]">
+            <form className="flex flex-col absolute top-[45%] left-1/2 -translate-y-1/2 -translate-x-1/2 m-auto border-2 border-[#ffffffb5] border-dashed rounded-3xl w-[55rem] h-[30rem] bg-[#1f1f1f3d] ">
                 <label htmlFor="FirstName" />
                 <input type="text" id="FirstName" />
                 <label htmlFor="LastName" />
