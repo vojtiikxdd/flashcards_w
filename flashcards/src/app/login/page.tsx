26da300418fde55394f495c65774038fbe4f0e13
vojtiikkxdd <vojtarazima@gmail.com>
2025-02-07 10:44:22 +0100

    adding first input to login page, styling it

--- flashcards/src/app/login/page.tsx
+++ flashcards/src/app/login/page.tsx
@@ -1,15 +1,16 @@
 export default function Login() {
     return (
         <main>
-            <form className="flex flex-col absolute top-[45%] left-1/2 -translate-y-1/2 -translate-x-1/2 m-auto border-2 border-[#ffffffb5] border-dashed rounded-3xl w-[55rem] h-[30rem] bg-[#1f1f1f3d] ">
-                <label htmlFor="FirstName" />
-                <input type="text" id="FirstName" />
-                <label htmlFor="LastName" />
-                <input type="text" id="LastName" />
-                <label htmlFor="Email" />
-                <input type="email" id="Email" />
-                <label htmlFor="Password" />
-                <input type="password" id="Password" />
+            <form className="flex flex-col absolute top-[45%] left-1/2 -translate-y-1/2 -translate-x-1/2 m-auto border-2 border-[#141414] border-dashed rounded-3xl w-[45rem] h-[25rem] bg-[#1f1f1f3d] ">
+                <div className="flex flex-row justify-center items-center mt-[2%] relative">
+                    <h1 className="text-4xl text-[#f1f1f1] text-center font-bold">Log-in! <span className="italic font-extralight">other</span></h1>
+                    <div className="absolute right-3 bg-gray-500 w-[100px] h-[30px] rounded-3xl"></div>
+                </div>
+                <div className="flex flex-row relative items-center justify-between">
+                    <label htmlFor="FirstName" className="absolute">Jméno</label>
+                    <input type="text" id="FirstName" className="mx-auto rounded w-96 h-10 mt-[6%]" />
+                </div>
+
             </form>
         </main>
     )
