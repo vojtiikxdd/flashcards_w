125858609437fc40a90f9f54747bf5d9a75ecefb
vojtiikkxdd <vojtarazima@gmail.com>
2025-02-11 22:13:37 +0100

    adding floating label to first input in login page, need to make all the other float too

--- flashcards/src/app/login/page.tsx
+++ flashcards/src/app/login/page.tsx
@@ -4,7 +4,22 @@ import React, { useState } from "react";
 import { supabase } from "../supabase";
 import bcrypt from "bcryptjs";
 
+//funkce pro pohybu labelu nad inputem
+export const useFloatingLabel = () => {
+  const [value, setValue] = useState("");
+  const [isTopped, setIsTopped] = useState(false);
 
+  const handleFocus = () => setIsTopped(true);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+    setIsTopped(e.target.value !== ""); // Ponechá label nahoře, pokud je input vyplněný
+  };
+
+  {/*Issue: Pokud se input vyplní, ale pak se smaže, label se vrátí dolů, ikdyž je input aktivní*/}
+  const handleBlur = () => setIsTopped(value !== ""); // Pokud je input prázdný, label se vrátí dolů
+
+  return { value, isTopped, handleFocus, handleChange, handleBlur };
+};
 
 export default function Login() {
   const [nickname, setNickname] = useState('')
@@ -33,9 +48,13 @@ export default function Login() {
     }
   }
 
-  const [register, setRegister] = useState(false);
-  const [inputValue, setInputValue] = useState("");
-  const [isFocused, setIsFocused] = useState(false);
+  const [register, setRegister] = useState(true);
+
+  // Vytvoření proměnných pro jednotlivé labely
+  const nickVal = useFloatingLabel();
+  const emailVal = useFloatingLabel();
+  const pswdVal = useFloatingLabel();
+  const confirmPswdVal = useFloatingLabel();
 
   return (
     <main className="absolute top-[45%] left-1/2 -translate-y-1/2 -translate-x-1/2 m-auto border-2 border-[#141414] border-dashed rounded-3xl w-[45rem] h-[25rem] bg-[#1f1f1f3d]">
@@ -66,28 +85,21 @@ export default function Login() {
         {register ? (
           <div className="relative flex flex-row justify-center items-center">
             <label htmlFor="LogFormInput" id="LogFormLabel"
-              className='absolute text-xl font-semibold text-[#cccccc] ease-in-out duration-300 bottom-[265px] top-[50px] left-[178px] bg-[#ffffff] rounded-xl labelMovement cursor-pointer select-none w-10 h-5' onClick={() => {
-                const logFormInput = document.getElementById('LogFormInput') as HTMLInputElement | null;
-                if (logFormInput) {
-                  logFormInput.oninput = (a) => {
-                    const target = a.target as HTMLInputElement;
-                    if (target && target.value !== '') {
-                      if (a.target) {
-                        (a.target as HTMLInputElement).classList.add('valid');
-                      }
-                    } else if (a.target && (a.target as HTMLInputElement).value === '') {
-                      (a.target as HTMLInputElement).classList.remove('valid');
-                    }
-                  };
-                }
-              }}>nickname</label>
+              className={`absolute text-xl font-semibold text-[#cccccc] ease-in-out duration-200 bottom-[265px] left-[178px] bg-[#201c35] cursor-pointer select-none w-[104px] h-6 flex items-center justify-center  ${nickVal.isTopped ? "top-[28px] text-lg left-[182px]" : "top-[50px]"}`} >nickname</label>
             <input
               type="text" id="LogFormInput"
-              value={nickname}
-              onChange={(e) => setNickname(e.target.value)}
-              className="items-center labelMovement mx-auto p-2 text-xl font-semibold rounded-xl w-96 h-10 mt-[6%] focus:outline-none outline-[#222222]  border-2 border-solid border-[#d7d7d7] focus:border-[#7c46a3] ease-in-out duration-300 bg-[transparent] selection:bg-[#101010] cursor-pointer focus:cursor-text hover:border-[#a255f0]" />
+              value={nickVal.value}
+              onChange={(e) => {
+                setNickname(e.target.value);
+                nickVal.handleChange(e);}
+              }
+              onFocus={nickVal.handleFocus}
+              onBlur={nickVal.handleBlur}
+
+              className="items-center mx-auto p-2 text-xl font-semibold rounded-xl w-96 h-10 mt-[6%] focus:outline-none outline-[#222222]  border-2 border-solid border-[#d7d7d7] focus:border-[#7c46a3] ease-in-out duration-300 bg-[transparent] selection:bg-[#101010] cursor-pointer focus:cursor-text hover:border-[#a255f0]" />
           </div>
         ) : null}
+
         <input
           type="email" id="emailInput" placeholder="email"
           value={email} onChange={(e) => setEmail(e.target.value)}
@@ -96,12 +108,15 @@ export default function Login() {
           type="password" id="passwordInput" placeholder="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          className="flex flex-row relative items-center justify-between labelMovement mx-auto p-2 text-xl font-semibold rounded-xl w-96 h-10 mt-[2%] focus:outline-none outline-[#222222]  border-2 border-solid border-[#d7d7d7] focus:border-[#7c46a3] ease-in-out duration-300 bg-[transparent] selection:bg-[#101010] cursor-pointer focus:cursor-text hover:border-[#a255f0]" />
+
+          className="flex flex-row relative items-center justify-between labelMovement mx-auto p-2 text-xl font-semibold rounded-xl w-96 h-10 mt-[2%] focus:outline-none outline-[#222222]  border-2 border-solid border-[#d7d7d7] focus:border-[#7c46a3] ease-in-out duration-300 bg-[transparent] selection:bg-[#101010] cursor-pointer focus:cursor-text hover:border-[#a255f0]" /> 
+
         {register ? (
           <input
             type="password" id="passwordInput" placeholder="password again"
             className="flex flex-row relative items-center justify-between labelMovement mx-auto p-2 text-xl font-semibold rounded-xl w-96 h-10 mt-[2%] focus:outline-none outline-[#222222]  border-2 border-solid border-[#d7d7d7] focus:border-[#7c46a3] ease-in-out duration-300 bg-[transparent] selection:bg-[#101010] cursor-pointer focus:cursor-text hover:border-[#a255f0]" />
         ) : null}
+
         <div className="mx-auto flex flex-row cursor-pointer relative items-center justify-between labelMovement">
           <button
             id="submitButton" type="submit"
