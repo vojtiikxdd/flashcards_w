d74f433ef40f5bce62311493abdddf1c19983277
vojtiikkxdd <vojtarazima@gmail.com>
2025-02-13 16:15:43 +0100

    making label movement work

--- flashcards/src/components/input.tsx
+++ flashcards/src/components/input.tsx
@@ -1,6 +1,9 @@
 "use client"
 
 import { useState } from "react";
+import { Eye } from "lucide-react";
+import { EyeOff } from "lucide-react";
+import { Info } from "lucide-react";
 
 type InputProps = {
     label: string;
@@ -10,20 +13,21 @@ type InputProps = {
 export function Input({ label, onValueChange, ...props }: InputProps) {
     const [isTopped, setIsTopped] = useState(false);
     const [isFocused, setIsFocused] = useState(false);
+    const [isPassword, setIsPassword] = useState(props.type);
 
     return (
         <div className="relative flex flex-row justify-center items-center">
+            {props.name === "password" && (
+                <div className="absolute left-[135px] top-[24px] cursor-pointer infoMessage">
+                    <Info />
+                </div>
+            )}
+            
             <label htmlFor={props.id}
             className={`absolute text-xl font-semibold text-[#cccccc] ease-in-out duration-200 bottom-[265px] left-[178px] bg-[#201c35] cursor-pointer select-none  h-6 flex items-center justify-center rounded-xl ${isTopped ? "top-[2px] text-lg left-[182px] px-2" : "top-[24px]"}`} >{label}</label>
             <input
                 onChange={(e) => {
-                    if (e.target.value.length > 0) {
-                        setIsTopped(true);
-                    } else if (e.target.value.length === 0 && isFocused) {
-                        setIsTopped(true);
-                    } else{
-                        setIsTopped(false);
-                    }
+                    {(e.target.value.length > 0 || (e.target.value.length === 0 && isFocused)) ? setIsTopped(true) : setIsTopped(false)}
                     onValueChange?.(e.target.value);
                 }}
                 onFocus={() => {
@@ -38,7 +42,26 @@ export function Input({ label, onValueChange, ...props }: InputProps) {
                 }}
                 id={props.id}
                 {...props}
-                className="enabled  items-center mx-auto p-2 text-xl text-[#ffffff] font-semibold rounded-xl w-96 h-10 mt-[16px] focus:outline-none outline-[#222222] border-2 border-solid border-[#d7d7d7] focus:border-[#7c46a3] ease-in-out duration-300 bg-transparent selection:bg-[#101010] cursor-pointer focus:cursor-text hover:border-[#a255f0]" />
+                type={isPassword}               
+                className="enabled  items-center mx-auto p-2 text-xl text-[#ffffff] font-semibold rounded-xl w-96 h-10 mt-[16px] focus:outline-none outline-[#222222] border-2 border-solid border-[#d7d7d7] focus:border-[#7c46a3] ease-in-out duration-300 bg-transparent selection:bg-[#101010] cursor-pointer focus:cursor-text hover:border-[#a255f0]" 
+            /> 
+                
+            {(props.name === "password" || props.name === "confirmPassword") && (isPassword === "password" || isPassword === "text") && (
+                <a className="absolute right-[180px] top-[24px] cursor-pointer"
+                    onClick={() => {
+                        if (isPassword === "password") {
+                            setIsPassword("text");
+                            props.type = "text";
+                        } else {
+                            setIsPassword("password");
+                            props.type = "password";
+                        }
+                    }}>
+                    {isPassword === "password" ? <Eye /> : <EyeOff />} 
+                </a>
+            )}
+
+            
         </div>
     )
 }
