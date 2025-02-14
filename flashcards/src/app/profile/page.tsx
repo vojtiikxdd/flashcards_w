42a73dd7d58bdb25af75f0ef483f7a76c0d85951
vojtiikkxdd <vojtarazima@gmail.com>
2025-02-14 15:53:27 +0100

    trying to add signout button/action, didnt get it

--- flashcards/src/app/profile/page.tsx
+++ flashcards/src/app/profile/page.tsx
@@ -1,31 +1,32 @@
-import Link from 'next/link';
-import {User} from 'lucide-react';
+"use client"
 
-export default function UserProfile() {
-    
-    const session = true;
+import { useEffect } from 'react';
+import { User } from "@/utils/schemas";
+import Link from 'next/link';
+
+export default function UserProfile({ user }: { user: User | string }) {
+
+    const session = typeof user === 'object';
     const FirstLetter = "A";
 
-    if (session) {
-      return (
+    useEffect(() => {
+        if (!session) {
+            window.location.href = '/login';
+        }
+    }, [session]);
+
+    return (
         <main>
-            <div className="flex absolute top-[45%] left-1/2 -translate-y-1/2 -translate-x-1/2 m-auto border-2 border-[#ffffff60] border-dashed rounded-3xl w-[55rem] h-[30rem] bg-[#1f1f1f3d]">
-              <div className='flex flex-col pl-8 pt-10'>
-                <div className='border-white border-solid rounded-full bg-[#000] w-24 h-24 flex justify-center items-center text-4xl text-bold'>
-                  {FirstLetter}
+            {session ? (
+                <div className="flex absolute top-[45%] left-1/2 -translate-y-1/2 -translate-x-1/2 m-auto border-2 border-[#ffffff60] border-dashed rounded-3xl w-[55rem] h-[30rem] bg-[#1f1f1f3d]">
+                    <div className='flex flex-col pl-8 pt-10'>
+                        <div className='border-white border-solid rounded-full bg-[#000] w-24 h-24 flex justify-center items-center text-4xl text-bold'>
+                            {FirstLetter}
+                        </div>
+                    </div>
+                    <div></div>
                 </div>
-              </div>
-              <div></div>
-            </div>
+            ) : null}
         </main>
-      );
-    }
-    else { 
-      return ( 
-          <main className="flex min-h-screen flex-col">
-              <h1 className="text-red-500">Ahoj</h1>
-              <Link href="/">zpatky</Link>
-          </main>
-      );
-  }
+    )
 }
