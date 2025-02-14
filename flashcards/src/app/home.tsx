aa9d3ce4b029b8449084380e16c19476eb82c1e8
vojtiikkxdd <vojtarazima@gmail.com>
2025-02-14 14:03:18 +0100

    Fix homepage

---
+++ flashcards/src/app/home.tsx
@@ -0,0 +1,26 @@
+import { User } from "@/utils/schemas";
+import Link from "next/link";
+
+export default function Home({ user }: { user: User | string }) {
+    const session = typeof user === 'object';
+
+    return (
+        <main className="flex flex-1 flex-col">
+            {session ? (
+                <div className="flex absolute top-1/3 left-1/2 -translate-y-1/2 -translate-x-1/2 m-auto justify-center items-center">
+                    <p className="text-[#fff] text-4xl font-medium relative">
+                        Welcome back, <span className="bg-clip-text from-[#5e25da] to-[#da25d4] bg-gradient-to-r text-transparent font-bold duration-200 transition-all ease-in-out">{user.nickname}</span>! <br />Wanna study sum?
+                    </p>
+                </div>
+            ) : (
+                <div className="flex absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 m-auto justify-center items-center">
+                    <p className="text-[#fff] text-5xl font-medium relative">
+                        <Link href="/login" className="bg-clip-text from-[#2592da] font-semibold to-[#da25d4] bg-gradient-to-r text-transparent hover:font-[900] duration-200 transition-all ease-in-out">
+                            Log-in
+                        </Link> and start learning with FlashCards!
+                    </p>
+                </div>
+            )}
+        </main>
+    )
+}
--- flashcards/src/app/page.tsx
+++ flashcards/src/app/page.tsx
@@ -1,28 +1,9 @@
-import Link from "next/link";
+import { getUser } from "@/utils/supabase/actions";
+import Home from "./home";
 
-export default function Home() {
-
-    const session = false;
-    const Name = "";
+export default async function HomeWrapper() {
+    const user = await getUser();
     
 
-    return (
-        <main className="flex flex-1 flex-col">
-            {session ? (
-                <div className="flex absolute top-1/3 left-1/2 -translate-y-1/2 -translate-x-1/2 m-auto justify-center items-center">
-                    <p className="text-[#fff] text-4xl font-medium relative">
-                        Welcome back, <span className="bg-clip-text from-[#5e25da] to-[#da25d4] bg-gradient-to-r text-transparent font-bold duration-200 transition-all ease-in-out">{Name}</span>! <br />Wanna study sum?
-                    </p>
-                </div>
-            ) : (
-                <div className="flex absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 m-auto justify-center items-center">
-                    <p className="text-[#fff] text-5xl font-medium relative">
-                        <Link href="/login" className="bg-clip-text from-[#2592da] font-semibold to-[#da25d4] bg-gradient-to-r text-transparent hover:font-[900] duration-200 transition-all ease-in-out">
-                            Log-in
-                        </Link> and start learning with FlashCards!
-                    </p>
-                </div>
-            )}
-        </main>
-    )
+    return <Home user={user} />;
 }
