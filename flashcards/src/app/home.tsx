163a2c03a16c577e120fe531ed0a0b0a77d3566a
vojtiikkxdd <vojtarazima@gmail.com>
2025-02-19 21:41:39 +0100

    creating createNew button

--- flashcards/src/app/home.tsx
+++ flashcards/src/app/home.tsx
@@ -1,18 +1,26 @@
 import { User } from "@/utils/schemas";
 
 import Link from "next/link";
+import { Plus } from "lucide-react";
 
 export default function Home({ user }: { user: User | string }) {
     const session = typeof user === 'object';
     
+    if(typeof user === 'object') console.log(user.nickname);
+    
     return (
         <main className="flex flex-1 flex-col">
             {session ? (
-                <div className="flex absolute top-1/3 left-1/2 -translate-y-1/2 -translate-x-1/2 m-auto justify-center items-center">
+                <div className="flex flex-col w-full absolute top-1/3 left-1/2 -translate-y-1/2 -translate-x-1/2 m-auto justify-center items-center">
                     <p className="text-[#fff] text-4xl font-medium relative">
-                        Welcome back, <span className="bg-clip-text from-[#5e25da] to-[#da25d4] bg-gradient-to-r text-transparent font-bold duration-200 transition-all ease-in-out">{user.nickname}</span>! <br />Wanna study sum?
+                        Welcome back, {user.nickname} <span className="bg-clip-text from-[#5e25da] to-[#da25d4] bg-gradient-to-r text-transparent font-bold duration-200 transition-all ease-in-out">{user.nickname}</span>! <br />Wanna study sum?
                     </p>
-                    
+                    <div className="flex flex-row mt-8 ml-12 self-start ">
+                        <Link href={"/createNew"} className="flex flex-row items-center bg-[#2592da] text-[#fff] text-lg px-2 py-1 rounded-3xl duration-200 transition-all ease-in-out hover:bg-[#0874bd]">
+                            <Plus size={30} className="pr-1"/>
+                            Create new!
+                        </Link>
+                    </div>
                 </div>
             ) : (
                 <div className="flex absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 m-auto justify-center items-center">
--- flashcards/src/app/page.tsx
+++ flashcards/src/app/page.tsx
@@ -4,6 +4,5 @@ import Home from "./home";
 export default async function HomeWrapper() {
     const user = await getUser();
     
-
     return <Home user={user} />;
 }
