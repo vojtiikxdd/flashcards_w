f2f68e60cd31ce9272abb478633a7f58bb27df81
vojtiikkxdd <vojtarazima@gmail.com>
2025-02-19 22:54:02 +0100

    creating new component for displaying recent flashcards and list all user's flashcards

---
+++ flashcards/src/components/flashcardsBox.tsx
@@ -0,0 +1,9 @@
+"use clinet";
+
+export function FlashcardsBox(){
+    return (
+        <div className="flex w-[220px] h-[160px] bg-[#16111f] rounded-3xl border-2 border-solid border-[#686868] p-4 text-3xl font-bold text-[#fff]">
+            kok
+        </div>
+    )
+}
