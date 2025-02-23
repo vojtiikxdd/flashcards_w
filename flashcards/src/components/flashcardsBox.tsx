f4b224226a574d2cf9e80cad5ad4f4361d763ed8
vojtiikkxdd <vojtarazima@gmail.com>
2025-02-23 22:25:47 +0100

    adding details (?)

--- flashcards/src/components/flashcardsBox.tsx
+++ flashcards/src/components/flashcardsBox.tsx
@@ -2,7 +2,7 @@
 
 export function FlashcardsBox(){
     return (
-        <div className="flex w-[220px] h-[160px] bg-[#16111f] rounded-3xl border-2 border-solid border-[#686868] p-4 text-3xl font-bold text-[#fff]">
+        <div className="flex w-[220px] h-[160px] bg-[#16111f] rounded-3xl border-2 border-solid border-[#686868] p-4 text-3xl font-bold text-[#fff] hover:border-[#969696] transition-colors ease-in-out duration-300 drop-shadow-sm hover:drop-shadow-white cursor-pointer">
             kok
         </div>
     )
