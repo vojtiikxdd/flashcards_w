f57e1384ae6d4e1a84a8dff3bb4cbc54c64679cd
majko13 <miklovicm.07@spst.eu>
2025-03-04 15:27:48 +0100

    animece flipu

--- flashcards/src/app/cards/page.tsx
+++ flashcards/src/app/cards/page.tsx
@@ -1,49 +1,9 @@
-// "use client";
-// import { Triangle } from "lucide-react";
-// import * as React from "react";
-// import {
-//   Carousel,
-//   CarouselContent,
-//   CarouselItem,
-//   CarouselNext,
-//   CarouselPrevious,
-// } from "@/components/ui/carousel";
 
-// import Link from "next/link";
-
-// export default function Cards() {
-//   return (
-//     <main>
-//       <Carousel className="text-white text-center flex absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 m-auto justify-center items-center border-2 border-[#ffffff60] border-dashed rounded-3xl w-[55rem] h-[30rem]">
-//         <CarouselContent>
-//           <CarouselItem className="carousel-item w-full p-4">
-//             <p className="text-lg w-full whitespace-normal break-words p-20">
-//               ahoijasdadasdasdsadasdasdasdasdasdsadasdasdsadasdasdsadsadasdasdasdassssssssssssssaaaaaaaaaasssssssssssssss
-//             </p>
-//           </CarouselItem>
-//           <CarouselItem className="carousel-item w-full p-4">
-//             <p className="text-lg w-full whitespace-normal break-words p-20">
-//               ahoijasdadasdasdsadasdasdasdasdasdsadasdasdsadasdasdsadsadasdasdasdassssssssssssssaaaaaaaaaasssssssssssssss
-//             </p>
-//           </CarouselItem>
-//           <CarouselItem className="carousel-item w-full p-4">
-//             <p className="text-lg w-full whitespace-normal break-words p-20">
-//               ahoijasdadasdasdsadasdasdasdasdasdsadasdasdsadasdasdsadsadasdasdasdassssssssssssssaaaaaaaaaasssssssssssssss
-//             </p>
-//           </CarouselItem>
-//         </CarouselContent>
-       
-//         <CarouselPrevious/>
-
-//         <CarouselNext/>
-          
-//       </Carousel>
-//     </main>
-//   );
-// }
 "use client";
 
-import * as React from "react";
+import React, { useState } from "react";
+import ReactCardFlip from "react-card-flip";
+
 import {
   Carousel,
   CarouselContent,
@@ -52,7 +12,12 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 
+
+
 export default function Cards() {
+
+    const [flip, setFlip] = useState(false);
+    
   const numbers = Array.from({ length: 10 }, (_, i) => i + 1); // [1, 2, ..., 10]
   const answers = numbers.map((num) => `Odpověď ${num}`); // ["Odpověď 1", ..., "Odpověď 10"]
   const [clickedIndex, setClickedIndex] = React.useState<number | null>(null);
@@ -71,20 +36,25 @@ export default function Cards() {
               className="carousel-item w-full p-4 cursor-pointer"
               onClick={() => handleClick(index)}
             >
-              {clickedIndex === index ? (
-                <p className="text-lg w-full whitespace-normal break-words p-20">
-                  {answers[index]} {/* Zobrazí odpověď */}
-                </p>
-              ) : (
-                <p className="text-lg w-full whitespace-normal break-words p-20">
-                  Položka {num} {/* Zobrazí číslo */}
-                </p>
-              )}
+            
+            <ReactCardFlip isFlipped={flip} 
+            flipDirection="vertical">
+            <p className=" text-lg w-full whitespace-normal break-words p-20"
+                 onClick={() => setFlip(!flip)}>
+                    Položka {num} 
+            </p>
+
+            <p className=" text-lg w-full whitespace-normal break-words p-20"
+                 onClick={() => setFlip(!flip)}>
+                    {answers[index]}
+            </p>
+            
+        </ReactCardFlip>
             </CarouselItem>
           ))}
         </CarouselContent>
-
-        <CarouselPrevious />
+        
+        <CarouselPrevious/>
         <CarouselNext />
       </Carousel>
     </main>
