d20b209add4db8af375af9040f88aff779b85ae4
vojtiikkxdd <vojtarazima@gmail.com>
2025-03-04 12:50:10 +0100

    adding count of added flashcards at the bottom

--- flashcards/src/components/createNew/flashcardEntry.tsx
+++ flashcards/src/components/createNew/flashcardEntry.tsx
@@ -1,7 +1,9 @@
 import { DivideIcon, Plus } from "lucide-react";
 import { FlashcardItem } from "./flashcardItem";
+import { useState } from "react";
 
 let id = 4;
+
 
 type Props = {
     bgColor: string;
@@ -10,7 +12,6 @@ type Props = {
 
 function IndexIdCreator() {
     id++;
-
     return id;
 }
 
@@ -24,6 +25,16 @@ function addItem(props: Props) {
 }
 
 export default function FlashcardEntry({ ...props }: Props) {
+    const [addCounter, setAddCounter] = useState(1);
+
+    function buttonCounter(e: React.MouseEvent<HTMLButtonElement>) {
+        if (e.currentTarget.textContent === "+") {
+            if (addCounter < 20) setAddCounter(addCounter + 1);
+        } else if (e.currentTarget.textContent === "-") {
+            if (addCounter > 1) setAddCounter(addCounter - 1);
+        }
+    }
+
     return (
         <div>
             <div className={`${props.bgColor} p-4 rounded-lg mt-4`}>
@@ -38,13 +49,42 @@ export default function FlashcardEntry({ ...props }: Props) {
             <div className={`${props.bgColor} p-4 rounded-lg mt-4`}>
                 <FlashcardItem entryIndex={4} txtareaBgCol="txtareaBgLightGrey" />
             </div>
-            <div className="flex justify-center mt-4">
-                <button
-                    onClick={() => addItem(props)} type="button"
-                    className="w-12 h-12 bg-blue-600 hover:bg-blue-700 text-white rounded-full flex items-center justify-center shadow-lg"
-                >
-                    <Plus size={30} />
-                </button>
+            <div className="flex flex-row justify-center mt-4">
+                <div className="py-2 px-8 rounded-full bg-[#3e3e3e] flex flex-row items-center gap-2">
+                    <button
+                        onClick={() => addItem(props)} type="button"
+                        className="w-12 h-12 bg-blue-600 hover:bg-blue-700 text-white rounded-full flex items-center justify-center shadow-lg"
+                    >
+                        <Plus size={30} />
+                    </button>
+                    <div className="flex flex-row items-center gap-2">
+                        <p className="font-bold text-xl">Add cards</p>
+                        <div className="flex flex-row items-center rounded-full bg-white gap-2">
+                            <input type="number"
+                                placeholder="1"
+                                value={addCounter}
+                                min={1}
+                                max={20}
+                                readOnly
+                                className="h-10 bg-transparent border-none text-base text-black w-16 active:outline-none focus:outline-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+                            />
+                            <div className="flex flex-col">
+                                <button type="button"
+                                    className="flex h-4 items-center text-black"
+                                    onClick={buttonCounter}
+                                >
+                                    +
+                                </button>
+                                <button type="button"
+                                    className="flex h-4 items-center text-black"
+                                    onClick={buttonCounter}
+                                >
+                                    -
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- flashcards/src/components/createNew/flashcardItem.tsx
+++ flashcards/src/components/createNew/flashcardItem.tsx
@@ -8,6 +8,7 @@ type Props = {
 export function FlashcardItem({ ...props }: Props) {
     return (
         <div className="pt-2">
+            {props.entryIndex}
             <SingleItem
                 label="Term"
                 name="term"
