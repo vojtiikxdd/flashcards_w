6d43f77956c968fa2e46164d6ade6b718d93135d
vojtiikkxdd <vojtarazima@gmail.com>
2025-03-05 09:40:36 +0100

    backup

--- flashcards/src/components/createNew/flashcardEntry.tsx
+++ flashcards/src/components/createNew/flashcardEntry.tsx
@@ -1,6 +1,7 @@
 import { DivideIcon, Plus } from "lucide-react";
 import { FlashcardItem } from "./flashcardItem";
 import { useState } from "react";
+import { NewCardCounter } from "./newCard&Counter";
 
 let id = 4;
 
@@ -15,17 +16,9 @@ function IndexIdCreator() {
     return id;
 }
 
-function addItem(props: Props) {
-    console.log(IndexIdCreator());
-    return (
-        <div className={`${props.bgColor} p-4 rounded-lg mt-4`}>
-            <FlashcardItem entryIndex={IndexIdCreator()} txtareaBgCol="txtareaBgLightGrey" />
-        </div>
-    );
-}
-
 export default function FlashcardEntry({ ...props }: Props) {
     const [addCounter, setAddCounter] = useState(1);
+    const [flashcards, setFlashcards] = useState<number[]>([1, 2, 3, 4]);
 
     function buttonCounter(e: React.MouseEvent<HTMLButtonElement>) {
         if (e.currentTarget.textContent === "+") {
@@ -33,6 +26,10 @@ export default function FlashcardEntry({ ...props }: Props) {
         } else if (e.currentTarget.textContent === "-") {
             if (addCounter > 1) setAddCounter(addCounter - 1);
         }
+    }
+
+    function addItem() {
+        setFlashcards((prev) => [...prev, IndexIdCreator()]);
     }
 
     return (
@@ -50,41 +47,7 @@ export default function FlashcardEntry({ ...props }: Props) {
                 <FlashcardItem entryIndex={4} txtareaBgCol="txtareaBgLightGrey" />
             </div>
             <div className="flex flex-row justify-center mt-4">
-                <div className="py-2 px-8 rounded-full bg-[#3e3e3e] flex flex-row items-center gap-2">
-                    <button
-                        onClick={() => addItem(props)} type="button"
-                        className="w-12 h-12 bg-blue-600 hover:bg-blue-700 text-white rounded-full flex items-center justify-center shadow-lg"
-                    >
-                        <Plus size={30} />
-                    </button>
-                    <div className="flex flex-row items-center gap-2">
-                        <p className="font-bold text-xl">Add cards</p>
-                        <div className="flex flex-row items-center rounded-full bg-white gap-2">
-                            <input type="number"
-                                placeholder="1"
-                                value={addCounter}
-                                min={1}
-                                max={20}
-                                readOnly
-                                className="h-10 bg-transparent border-none text-base text-black w-16 active:outline-none focus:outline-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
-                            />
-                            <div className="flex flex-col">
-                                <button type="button"
-                                    className="flex h-4 items-center text-black"
-                                    onClick={buttonCounter}
-                                >
-                                    +
-                                </button>
-                                <button type="button"
-                                    className="flex h-4 items-center text-black"
-                                    onClick={buttonCounter}
-                                >
-                                    -
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <NewCardCounter addCounter={addCounter} setAddCounter={setAddCounter} buttonCounter={buttonCounter} addItem={addItem} />
             </div>
         </div>
     );
